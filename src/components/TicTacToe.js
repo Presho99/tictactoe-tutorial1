e8ff598bd1b3de6f455f3e2469c4817262f1cce1
Precious <Preshomondi@gmail.com>
2022-11-14 21:24:40 +0300

    create object holding names and symbols of players

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import "../index.css"
+import {useEffect, useState} from "react"
 
 function TicTacToe() {
+    // object holding name and symbol of each player in the game
+    const players ={
+        CPU: {
+            SYM: "O",
+            NAME: "CPU",
+        },
+        HUMAN: {
+            SYM: "X",
+            NAME: "You",
+        },
+    };
   return (
     <div className='tictac'>
         game here
